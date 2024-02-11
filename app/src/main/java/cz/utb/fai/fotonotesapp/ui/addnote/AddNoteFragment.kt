16bf974a5abc0e7c842f4e8cf5f371d0ec357bf2
Karol Cáb <k_cab@utb.cz>
2024-02-11 14:48:47 +0100

    updated addNote functionality

--- app/src/main/java/cz/utb/fai/fotonotesapp/ui/addnote/AddNoteFragment.kt
+++ app/src/main/java/cz/utb/fai/fotonotesapp/ui/addnote/AddNoteFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -28,10 +29,25 @@ class AddNoteFragment : Fragment() {
         _binding = FragmentAddnoteBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        /*val textView: TextView = binding.textNotifications
-        addNoteViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }*/
+        val clearButton: Button = binding.clearButton
+        val saveButton : Button = binding.saveButton
+
+        clearButton.setOnClickListener {
+            addNoteViewModel.clearNote()
+        }
+
+        saveButton.setOnClickListener {
+            addNoteViewModel.createAndSaveNote(
+                binding.editNoteTitle.text.toString(),
+                binding.editTextTextMultiLine.text.toString()
+            )
+        }
+
+        addNoteViewModel.note.observe(viewLifecycleOwner) {
+            binding.editNoteTitle.setText(it.title)
+            binding.editTextTextMultiLine.setText(it.content)
+        }
+
         return root
     }
 
--- app/src/main/java/cz/utb/fai/fotonotesapp/ui/addnote/AddNoteViewModel.kt
+++ app/src/main/java/cz/utb/fai/fotonotesapp/ui/addnote/AddNoteViewModel.kt
@@ -3,11 +3,26 @@ package cz.utb.fai.fotonotesapp.ui.addnote
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import cz.utb.fai.fotonotesapp.model.Note
 
 class AddNoteViewModel : ViewModel() {
+    private var _note = MutableLiveData<Note>()
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+    var note: LiveData<Note> = _note
+
+    fun createAndSaveNote(title: String, content: String) {
+        val newNote = Note(title, content, System.currentTimeMillis())
+        saveNote(newNote)
+        _note.value = newNote
+
     }
-    val text: LiveData<String> = _text
+
+    fun saveNote(note: Note) {
+        //saving data
+    }
+
+    fun clearNote() {
+        _note.value = Note("", "", System.currentTimeMillis())
+    }
+
 }
