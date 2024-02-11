7caa50a63bbd54ebc91dd6a0aa63ba4a598cdda3
Karol Cáb <k_cab@utb.cz>
2024-02-11 21:05:44 +0100

    Added camera fragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,19 @@ dependencies {
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
     implementation("androidx.datastore:datastore-preferences:1.0.0")
     implementation("com.google.code.gson:gson:2.10.1")
+    implementation("androidx.compose.material3:material3:1.2.0")
+    implementation("androidx.compose.material:material-icons-extended-android:1.6.1")
+
+    val cameraVersion = "1.3.1"
+    implementation("androidx.camera:camera-core:$cameraVersion")
+    implementation("androidx.camera:camera-camera2:$cameraVersion")
+    implementation("androidx.camera:camera-lifecycle:$cameraVersion")
+    implementation("androidx.camera:camera-video:$cameraVersion")
+    implementation("androidx.camera:camera-view:$cameraVersion")
+    implementation("androidx.camera:camera-extensions:$cameraVersion")
+
+
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/cz/utb/fai/fotonotesapp/ui/camera/CameraFragment.kt
+++ app/src/main/java/cz/utb/fai/fotonotesapp/ui/camera/CameraFragment.kt
@@ -1,10 +1,16 @@
 package cz.utb.fai.fotonotesapp.ui.camera
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.camera.view.CameraController
+import androidx.camera.view.LifecycleCameraController
+import androidx.camera.view.PreviewView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import cz.utb.fai.fotonotesapp.databinding.FragmentCameraBinding
@@ -13,9 +19,27 @@ class CameraFragment : Fragment() {
 
     private var _binding: FragmentCameraBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    private fun hasRequiredPersmissions(): Boolean {
+        return CAMERAX_PERMISSIONS.all {
+            if(activity != null) {
+                false
+            }
+            else {
+                ContextCompat.checkSelfPermission(
+                    requireActivity().applicationContext,
+                it
+                ) == PackageManager.PERMISSION_GRANTED
+            }
+        }
+    }
+    companion object {
+        private val CAMERAX_PERMISSIONS = arrayOf(
+            Manifest.permission.CAMERA,
+            Manifest.permission.RECORD_AUDIO,
+        )
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,10 +52,23 @@ class CameraFragment : Fragment() {
         _binding = FragmentCameraBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        cameraViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        if(!hasRequiredPersmissions()) {
+            ActivityCompat.requestPermissions(
+                requireActivity(), CAMERAX_PERMISSIONS, 0
+            )
         }
+
+        val controller = LifecycleCameraController(requireContext()).apply {
+            setEnabledUseCases(
+                CameraController.IMAGE_CAPTURE or
+                CameraController.VIDEO_CAPTURE
+            )
+        }
+
+        val previewView: PreviewView = binding.cameraView
+        previewView.controller = controller
+        (previewView.controller as LifecycleCameraController).bindToLifecycle(viewLifecycleOwner)
+
         return root
     }
 
